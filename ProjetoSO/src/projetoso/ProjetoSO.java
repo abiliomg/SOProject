6c6138e74598b27bd5db509619e121ba23f1803a
Abílio <abilio_mg_castro@hotmail.com>
2018-12-22 18:59:53 +0000

    Alterações no Elevador

--- ProjetoSO/src/projetoso/ProjetoSO.java
+++ ProjetoSO/src/projetoso/ProjetoSO.java
@@ -6,9 +6,7 @@
 package projetoso;
 
 import java.awt.Color;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JButton;
+import java.awt.Dimension;
 import javax.swing.*;
 import java.awt.GridLayout;
 
@@ -24,40 +22,65 @@ public class ProjetoSO {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        int i = 0;
+        int i = 0,piso,gridHelper;
         
         //Ler ficheiro com o número de andares e peso máximo
         FileManager fm = new FileManager();
         fm.readFile("elevador.txt");
-
-        JFrame elevador = new JFrame("Elevador");
         JPanel panela = new JPanel();
-
-        //Não sei o que faz
-        // elevador.setLayout(new GridLayout(2, 2));
-        panela.setLayout(new GridLayout(2, 2));
-        panela.setSize(250, 350);
-
+        Dimension panelDimension=new Dimension(300,300);
+        if(fm.getPisos()%2==0){
+            gridHelper=fm.getPisos()/2;
+        }else{
+            gridHelper=(fm.getPisos()+1)/2;
+        }
+        panela.setLayout(new GridLayout(3+gridHelper, 2));
+        
+        panela.setSize(panelDimension);
+        panela.setMaximumSize(panelDimension);
+        Dimension frameDimension=new Dimension(400,300);
+        
+        JFrame elevador = new JFrame("Elevador");
         //Definir tamanho da janela e localização inicial
-        elevador.setSize(400, 800);
-        elevador.setLocation(1920 / 2 - 300, 1080 / 2 - 400);
-
+        elevador.setSize(frameDimension);
+        elevador.setMaximumSize(frameDimension);
+        elevador.setLocation(1920 / 2 - 400, 1080 / 2 - 300);
         // Construtor do label que apresenta o andar atual
         JLabel andarAtual = new JLabel("Andar atual: ");
+        andarAtual.setHorizontalAlignment(SwingConstants.RIGHT);
         panela.add(andarAtual);
-        andarAtual.setSize(200, 200);
-
-        //Cicloque acrecenta botões dependendo do número de andares definido
+        andarAtual.setSize(30, 30);
+        JLabel andarMostrar=new JLabel(""+1);
+        panela.add(andarMostrar);
+        //Ciclo que acrecenta botões dependendo do número de andares definido
         JButton[] bt = new JButton[fm.getPisos()];
+        JButton fechar=new JButton("Fechar");
+        JButton abrir=new JButton("Abrir");
+        Dimension buttonDimension=new Dimension(10,10);
+        JLabel clear=new JLabel(" ");
+        JLabel clear2=new JLabel(" ");
+        JLabel clear3=new JLabel(" ");
         while (i < fm.getPisos()) {
-            bt[i] = new JButton("" + i);
-            bt[i].setBounds(0, i * 75, 70, 70);
+            piso=i+1;
+            bt[i] = new JButton(""+piso);
+            bt[i].setSize( 10, 10);
+            bt[i].setMaximumSize(buttonDimension);
             panela.add(bt[i]);
             i++;
         }
-
+        if(fm.getPisos()%2==0){
+            panela.add(clear);
+        panela.add(clear2);
+        }else{
+              panela.add(clear);
+        panela.add(clear2);
+                panela.add(clear3);
+                }
+        
+        panela.add(fechar);
+        panela.add(abrir);
         elevador.add(panela);
-        elevador.setBackground(Color.green);
+        elevador.setBackground(Color.GREEN);
         elevador.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         elevador.setVisible(true);
 
@@ -67,3 +90,4 @@ public class ProjetoSO {
     }
 
 }
+
